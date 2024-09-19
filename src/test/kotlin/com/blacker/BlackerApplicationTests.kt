101e8a8bd47af9b0b44104467493245687bbb7c5
class-yoo <mynameisyjh@gmail.com>
2024-09-19 20:19:16 +0900

    Init project & hello world

---
+++ src/test/kotlin/com/blacker/BlackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blacker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlackerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
